4d02eea2ec250373576a76d1ecbe22e578986228
Your Name <you@example.com>
2023-02-02 14:12:34 +0900

    first commit

---
+++ javascripts/ajax_sample.js
@@ -0,0 +1,24 @@
+function ajax() {
+  let number = 0;
+  const btn = document.getElementById('btn');
+  btn.addEventListener('click', e => {
+    const req = new XMLHttpRequest();
+    req.onreadystatechange = function() {
+      if (req.readyState == 4) {
+        if(req.status == 200) {
+          const videoArea = document.getElementById("video");
+          const titleArea = document.getElementById("title");
+          const contentArea = document.getElementById("content");
+          videoArea.innerHTML = req.response[number].url;
+          titleArea.innerHTML = req.response[number].title;
+          contentArea.innerHTML = req.response[number].content;
+          number == 2 ? number = 0 : number++
+        }
+      }
+    }
+    req.open("GET", "url.json");
+    req.responseType = "json"
+    req.send(null);
+  })
+}
+window.onload = ajax;
