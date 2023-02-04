a216b1fea172adf444f70768b1e609ad1d4b2795
Your Name <you@example.com>
2023-02-04 15:02:11 +0900

    frist commit

--- javascripts/ajax_sample.js
+++ javascripts/ajax_sample.js
@@ -1,24 +1,27 @@
-function ajax() {
-  let number = 0;
-  const btn = document.getElementById('btn');
-  btn.addEventListener('click', e => {
-    const req = new XMLHttpRequest();
-    req.onreadystatechange = function() {
-      if (req.readyState == 4) {
-        if(req.status == 200) {
-          const videoArea = document.getElementById("video");
-          const titleArea = document.getElementById("title");
-          const contentArea = document.getElementById("content");
-          videoArea.innerHTML = req.response[number].url;
-          titleArea.innerHTML = req.response[number].title;
-          contentArea.innerHTML = req.response[number].content;
-          number == 2 ? number = 0 : number++
-        }
-      }
-    }
-    req.open("GET", "ajax.json");
-    req.responseType = "json"
-    req.send(null);
-  })
+let number = 0;
+let data = [getData]; // ajax.jsonから取得したデータを格納するための変数を追加
+const button = document.getElementById('btn');
+const titleArea = document.getElementById("title");
+const contentArea = document.getElementById("content");
+const videoArea = document.getElementById("video");
+
+function getData() {
+  $.ajax('ajax.json',{
+    success: function(data){
+      return data; 
+     }}) // ajax.jsonからデータを取得する処理を記述
 }
-window.onload = ajax;
+
+function changeVideo() {
+  button.click(function(){
+    console.log("test");
+   // const videoData = getData(); 
+   // videoArea.html(videoData[number].url);
+   // titleArea.html(videoData[number].title);
+   // contentArea.html(videoData[number].content);
+   // number == 2 ? number = 0 : number++;
+  });// ボタンがクリックされた際の処理を記述
+  // ajax.jsonからデータを取得していない場合のみ、getDataの処理を呼び出す
+}
+
+window.onload = changeVideo;
