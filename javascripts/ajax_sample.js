164bf32885802250475334f77e2bb7762557c97d
Your Name <you@example.com>
2023-02-04 21:24:30 +0900

    first commit

--- javascripts/ajax_sample.js
+++ javascripts/ajax_sample.js
@@ -1,27 +1,31 @@
 let number = 0;
-let data = [getData]; // ajax.jsonから取得したデータを格納するための変数を追加
+let data = [getData]; 
 const button = document.getElementById('btn');
 const titleArea = document.getElementById("title");
 const contentArea = document.getElementById("content");
 const videoArea = document.getElementById("video");
-
 function getData() {
   $.ajax('ajax.json',{
     success: function(data){
       return data; 
-     }}) // ajax.jsonからデータを取得する処理を記述
+     }}) 
 }
-
 function changeVideo() {
-  button.click(function(){
-    console.log("test");
-   // const videoData = getData(); 
-   // videoArea.html(videoData[number].url);
-   // titleArea.html(videoData[number].title);
-   // contentArea.html(videoData[number].content);
-   // number == 2 ? number = 0 : number++;
-  });// ボタンがクリックされた際の処理を記述
-  // ajax.jsonからデータを取得していない場合のみ、getDataの処理を呼び出す
+  button.addEventListener('click', e => {
+    const request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+      if (request.readyState == 4) {
+        if(request.status == 200) {
+          titleArea.innerHTML = request.response[number].title;
+          contentArea.innerHTML = request.response[number].content;
+          videoArea.setAttribute("src", request.response[number].url);
+          number == 2 ? number = 0 : number++;
+        }
+      }
+    }
+    request.open("GET", "ajax.json");
+    request.responseType = "json";
+    request.send(null);
+  })
 }
-
 window.onload = changeVideo;
